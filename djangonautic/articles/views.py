5b3914d7f3c7709254fa59514dc7e1a982f7b50e
UBONGO2000 <gadyubongo@gmail.com>
2025-05-19 13:03:44 +0200

    second commit: creation des dossiers templates,app articles et configurations de quelques urls

---
+++ djangonautic/articles/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+# Create your views here.
+
+def home(request):
+    return render(request,"index.html")
+
+
+def articles_list(request):
+    return render(request, "articles/articles_list.html")
